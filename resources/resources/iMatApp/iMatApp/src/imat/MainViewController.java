f9e3d6be3747fb1f60ab743c2b91b01e5a349ce2
MarcoTaf <112405603+MarcoTaf@users.noreply.github.com>
2023-05-16 10:29:56 +0200

    Updated size to StartVy

--- resources/resources/iMatApp/iMatApp/src/imat/MainViewController.java
+++ resources/resources/iMatApp/iMatApp/src/imat/MainViewController.java
@@ -1,6 +1,7 @@
 
 package imat;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -8,10 +9,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 
-public class MainViewController implements Initializable {
 
+
+public class MainViewController implements Initializable {
+    @FXML
+    public StackPane startViewStackPane;
     @FXML
     Label pathLabel;
 
@@ -19,11 +25,23 @@ public class MainViewController implements Initializable {
 
     public void initialize(URL url, ResourceBundle rb) {
 
+        try {
+            startViewStackPane.getChildren().setAll(FXMLLoader.load(getClass().getResource("main.fxml")));
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
         String iMatDirectory = iMatDataHandler.imatDirectory();
 
         pathLabel.setText(iMatDirectory);
     }
 
+    private void loadViews()
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StartVy.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+    }
 
 
 }
