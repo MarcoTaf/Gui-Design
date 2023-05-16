05550fdfe7e4d626be7122b140762739d119d407
MarcoTaf <112405603+MarcoTaf@users.noreply.github.com>
2023-05-16 13:26:40 +0200

    Backend working!

--- resources/resources/iMatApp/iMatApp/src/imat/StartViewController.java
+++ resources/resources/iMatApp/iMatApp/src/imat/StartViewController.java
@@ -1,16 +1,20 @@
 package imat;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
 public class StartViewController extends AnchorPane {
+
+    @FXML
+    public Button startShoppingButton;
     public StartViewController()
     {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Startvy.fxml"));
-
-
+        fxmlLoader.setRoot(this);
 
         try {
             fxmlLoader.load();
